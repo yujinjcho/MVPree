162782f8d863b6b6317726dbde2b0c4006713deb
Yujin <yujinjcho@gmail.com>
2016-11-03 17:16:56 -0700

    Set up modules and packages

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+
+var React = require('react');
+
+
+var Main = React.createClass({
+  render: function() {
+
+    return (
+      <div>Hi</div>
+    );
+  }
+});
+
+module.exports = Main;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Main = require('./components/Main');
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+);
