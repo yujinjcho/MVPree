daf0724298448433b7f102d8d9a5efb7df0b1145
Yujin <yujinjcho@gmail.com>
2016-11-03 19:07:03 -0700

    Change name of Navbar to NavbarContainer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
-
 var React = require('react');
-
+var NavbarContainer = require('./NavbarContainer');
+require('../../static/css/main-container.css');
 
 var Main = React.createClass({
   render: function() {
 
     return (
-      <div>Hi</div>
+      <div className='main-container'>
+        <NavbarContainer/>
+      </div>
     );
   }
 });
